cdf11c1826b0da2297ba63cdc901ed92ac8ac0a0
JihwanB <93907076+JihwanB@users.noreply.github.com>
2024-01-16 18:38:20 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+        StringBuilder dog = new StringBuilder();
+
+        dog.append("|\\_/|\n");
+        dog.append("|q p|   /}\n");
+        dog.append("( 0 )\"\"\"\\\n");
+        dog.append("|\"^\"`    |\n");
+        dog.append("||_/=\\\\__|\n");
+        
+        System.out.println(dog);
+
+    }
+}
