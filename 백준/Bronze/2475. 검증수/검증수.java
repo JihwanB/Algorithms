8046225e7c7ed0272c3e18a6006442d08c8fca71
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-02-01 23:53:04 +0900

    [Bronze V] Title: 검증수, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+            arr[i] *= arr[i];
+            sum += arr[i];
+        }
+
+        System.out.println(sum % 10);
+
+    }
+}
