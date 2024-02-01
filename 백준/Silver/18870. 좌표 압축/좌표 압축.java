632bc4f65e065f5f89b270be7245c3b89729ef39
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-02-01 20:27:19 +0900

    [Silver II] Title: 좌표 압축, Time: 1880 ms, Memory: 242196 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
@@ -33,12 +32,11 @@ public class Main {
             }
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int key : origin) {
             int ranking = map.get(key);
-            bw.write(ranking + " ");
+            sb.append(ranking).append(" ");
         }
-
-        bw.flush();
-        
+        System.out.println(sb);
     }
 }
