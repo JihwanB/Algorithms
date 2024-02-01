a9873d687c27dcbe28d1eee3a8463c4e13814952
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-02-02 00:12:09 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 364 ms, Memory: 18676 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
