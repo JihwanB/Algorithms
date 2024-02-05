f36c0300ebd9fc2a8a4a5a4bb39e0f3cd2dff898
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-02-06 00:46:25 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 2636 ms, Memory: 168128 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> o2 - o1);
+
+        for (Integer i : arr) {
+            bw.write(i + "\n");
+        }
+
+        bw.close();
+
+    }
+
+}
