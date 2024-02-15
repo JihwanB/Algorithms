3769cc2dce7c5399ad66a622bd6753428b55b308
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-02-16 00:23:37 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 272 ms, Memory: 33180 KB -BaekjoonHub

---
+++ 백준/Silver V/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('<');
+
+        while (queue.size() > 1) {
+            for (int i = 0; i < K - 1; i++) {
+                int num = queue.poll();
+                queue.offer(num);
+            }
+            sb.append(queue.poll()).append(", ");
+        }
+
+        sb.append(queue.poll()).append('>');
+        System.out.println(sb);
+
+    }
+
+}
