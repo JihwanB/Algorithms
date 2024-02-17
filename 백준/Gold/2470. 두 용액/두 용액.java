96ebfefc22b7bfd4ad077faaff9f9be3fea1cc1d
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-02-18 02:02:39 +0900

    [Gold V] Title: 두 용액, Time: 420 ms, Memory: 28912 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,36 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
-class Main
-{
-    public static void main (String[] args) throws IOException {
+public class Main {
+
+    static int findOptimalPairValue(int[] arr, int fromIndex, int toIndex, int value) {
+        int optimalPairAbs = Integer.MAX_VALUE;
+        int optimalPairValue = 0;
+        int l = fromIndex;
+        int r = toIndex;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+            int sum = value + arr[m];
+            int sumAbs = Math.abs(sum);
+            if (optimalPairAbs > sumAbs) {
+                optimalPairValue = arr[m];
+                optimalPairAbs = sumAbs;
+            }
+            if (sum < 0) l = m + 1;
+            else if (sum > 0) r = m - 1;
+            else return arr[m];
+        }
+        return optimalPairValue;
+    }
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-
-        int ansAbs = 2000000001;
-        int ans1 = 0;
-        int ans2 = 0;
-        TreeSet<Integer> set = new TreeSet<>();
+        int[] solution = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            Integer[] pairValues = {set.floor(-x), set.ceiling(-x)};
-            for (Integer pairValue : pairValues) {
-                if (pairValue == null) continue;
-                int sumAbs = Math.abs(x + pairValue);
-                if (ansAbs > sumAbs) {
-                    ansAbs = sumAbs;
-                    ans1 = x;
-                    ans2 = pairValue;
-                }
-            }
-            set.add(x);
+            solution[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(Math.min(ans1, ans2) + " " + Math.max(ans1, ans2));
+
+        Arrays.sort(solution);
+
+        int ansAbs = Integer.MAX_VALUE;
+        int ans1 = -1;
+        int ans2 = -1;
+        for (int i = 0; i < N - 1; i++) {
+            int pairValue = findOptimalPairValue(solution, i + 1, N - 1, solution[i]);
+            int abs = Math.abs(solution[i] + pairValue);
+            if (abs < ansAbs) {
+                ansAbs = abs;
+                ans1 = solution[i];
+                ans2 = pairValue;
+            }
+        }
+
+        System.out.println(ans1 + " " + ans2);
+
     }
+
 }
