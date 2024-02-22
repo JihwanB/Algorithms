b8bb428875a743b1583f9e15d63da15919177f50
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-02-22 20:45:41 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        while (N-- > 0) {
+            sb.append(isVPS(br.readLine())).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    public static String isVPS(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '(')
+                stack.push(c);
+            else if (stack.empty())
+                return "NO";
+            else
+                stack.pop();
+        }
+
+        if (stack.empty())
+            return "YES";
+        else
+            return "NO";
+    }
+
+}
