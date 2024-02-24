1367abd6520c3debc5fd8e37aee2549465b1aadd
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-02-24 23:52:33 +0900

    [Bronze II] Title: 초6 수학, Time: 140 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/2702. 초6 수학/초6 수학.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int a, b;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            sb.append(getLCM(a, b)).append(" ").append(getGCF(a, b)).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    public static int getGCF(int a, int b) {
+        if (b == 0)
+            return a;
+        return getGCF(b, a % b);
+    }
+
+    public static int getLCM(int a, int b) {
+        return a * b / getGCF(a, b);
+    }
+
+}
