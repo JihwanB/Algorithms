5de5ce48701c9f1a70b132936b3929ba99081de7
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-03-02 20:51:32 +0900

    [Silver V] Title: 영화감독 숌, Time: 296 ms, Memory: 86376 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int ans = 666;
+        int count = 1;
+
+        while (count != n) {
+            ans++;
+            if(String.valueOf(ans).contains("666"))
+                count++;
+        }
+
+        System.out.println(ans);
+
+    }
+
+}
