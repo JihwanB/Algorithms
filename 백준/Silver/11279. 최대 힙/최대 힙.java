e751a94ea590413f06b9c454ed543fb6000b2936
Jihwan Baek <93907076+JihwanB@users.noreply.github.com>
2024-04-08 20:32:18 +0900

    [Silver II] Title: 최대 힙, Time: 344 ms, Memory: 26312 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> o2 - o1);
+
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if (x == 0) {
+				if (priorityQueue.isEmpty()) {
+					sb.append(0).append("\n");
+				} else {
+					sb.append(priorityQueue.poll()).append("\n");
+				}
+			} else {
+				priorityQueue.add(x);
+			}
+		}
+
+		System.out.println(sb);
+
+	}
+
+}
