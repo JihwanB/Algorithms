1487a54bba58d87f554fb71aee14fda1933fc4fa
JihwanB <93907076+JihwanB@users.noreply.github.com>
2025-04-09 18:37:26 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 476 ms, Memory: 117060 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[][] graph;
+    static boolean[] visited;
+    static int cnt;
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        graph = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int src = Integer.parseInt(st.nextToken());
+            int dst = Integer.parseInt(st.nextToken());
+            graph[src][dst] = 1;
+            graph[dst][src] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfsRecursion(i);
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    // DFS using recursion
+    static void dfsRecursion(int node) {
+        visited[node] = true;
+        for (int i = 1; i <= n; i++) {
+            if (graph[node][i] == 1 && !visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    // DFS using stack
+    static void dfs(int node) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(node);
+        visited[node] = true;
+
+        while (!stack.empty()) {
+            int cur = stack.pop();
+            for (int i = 1; i <= n; i++) {
+                if (graph[cur][i] == 1 && !visited[i]) {
+                    stack.push(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+
+    // BFS using queue
+    static void bfs(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
+        visited[node] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int i = 1; i <= n; i++) {
+                if (graph[cur][i] == 1 && !visited[i]) {
+                    queue.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+
+
+}
