37dc0e979c3733156819551f1fc0489ba5bf3c25
JihwanB <93907076+JihwanB@users.noreply.github.com>
2025-04-11 00:07:52 +0900

    [Gold IV] Title: 빙산, Time: 424 ms, Memory: 26684 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,88 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+
     static int n, m;
     static int[][] earth;
     static boolean[][] visited;
     static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1 ,1};
-    static List<Ice> iceList = new ArrayList<>();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+    static int[] dc = {0, 0, -1, 1};
+    static List<Ice> iceList;
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         earth = new int[n][m];
         visited = new boolean[n][m];
         iceList = new ArrayList<>();
 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                earth[i][j] = sc.nextInt();
-                if(earth[i][j] > 0) {
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                earth[i][j] = Integer.parseInt(st.nextToken());
+                if (earth[i][j] > 0) {
                     iceList.add(new Ice(i, j, earth[i][j]));
                 }
-                // iceList에 있는 빙산들의 좌표를 방문처리
                 visited[i][j] = true;
             }
         }
-        for(int year = 1; !iceList.isEmpty(); year++) {
-            // 빙산 녹이기
-            for(int i = 0; i < iceList.size(); i++) {
-                Ice ice = iceList.get(i);
-                for(int j = 0; j < 4; j++) {
-                    int nr = ice.row + dr[j];
-                    int nc = ice.col + dc[j];
-                    if(earth[nr][nc] == 0) ice.height--;
+
+        for (int year = 1; !iceList.isEmpty(); year++) {
+            for (Ice ice : iceList) {
+                for (int j = 0; j < 4; j++) {
+                    int nextRow = ice.row + dr[j];
+                    int nextCol = ice.col + dc[j];
+                    if (earth[nextRow][nextCol] == 0) {
+                        ice.height--;
+                    }
                 }
             }
 
-            // 빙산 녹은 후 높이 갱신
-            for(int i = 0; i < iceList.size(); i++) {
+            for (int i = 0; i < iceList.size(); i++) {
                 Ice ice = iceList.get(i);
-                if(ice.height <= 0) {
+                if (ice.height <= 0) {
                     earth[ice.row][ice.col] = 0;
-                    iceList.set(i, iceList.get(iceList.size()-1));
-                    iceList.remove(iceList.size()-1);
+                    iceList.set(i, iceList.get(iceList.size() - 1));
+                    iceList.remove(iceList.size() - 1);
                     i--;
                 }
-                // DFS 탐색 대상 -> visited 초기화
                 else {
                     visited[ice.row][ice.col] = false;
                 }
             }
 
-            // iceList 첫번째 빙산이 몇개와 연결되어있는지 카운트
-            // 모든 빙산의 개수와, 첫번째 빙산과 연결된 빙산의 개수가 다르면 빙산이 분리되었다는 뜻
-            if(iceList.size() > 0 && dfs(iceList.get(0).row, iceList.get(0).col) != iceList.size()) {
+            if (iceList.size() > 0
+                && dfs(iceList.get(0).row, iceList.get(0).col) != iceList.size()) {
                 System.out.println(year);
                 System.exit(0);
             }
         }
         System.out.println(0);
     }
-    static int dfs(int r, int c) {
-        visited[r][c] = true;
+
+    static int dfs(int row, int col) {
+        visited[row][col] = true;
         int cnt = 1;
-        for(int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-            if(visited[nr][nc]) continue;
-            cnt += dfs(nr, nc);
+        for (int i = 0; i < 4; i++) {
+            int nextRow = row + dr[i];
+            int nextCol = col + dc[i];
+            if (visited[nextRow][nextCol]) {
+                continue;
+            }
+            cnt += dfs(nextRow, nextCol);
         }
         return cnt;
     }
+
 }
 
 class Ice {
+
     int row;
     int col;
     int height;
-    public Ice(int r, int c, int h) {
-        this.row = r;
-        this.col = c;
-        this.height = h;
+
+    public Ice(int row, int col, int height) {
+        this.row = row;
+        this.col = col;
+        this.height = height;
     }
 }
